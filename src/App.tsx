d2ed9c348e12d920dd87d302308e677d80b615c4
jhlee <wisdom_13_@naver.com>
2023-03-19 23:16:10 +0900

    라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from './page/MainPage';
+import MovieDetail from './page/MovieDetail';
+import TvDetail from './page/TvDetail';
+import TvPage from './page/TvPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/tv" element={<TvPage />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/tv/:id" element={<TvDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/page/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MainPage: React.FC = () => {
+  return (
+    <div>mainpage</div>
+  )
+}
+
+export default MainPage;
